f9fcd5250abb2e350acc3d52ed1ef5a57f436d32
Shorin Sergey <serres123@ya.ru>
2021-11-28 00:34:47 +0300

    [algo_1] finish seminar hm

---
+++ algo_1/seminar_hm/dtw.py
@@ -0,0 +1,93 @@
+
+
+DEBUG_PRINT_DYNAMIC_ARR = False
+
+
+def linear_distance(x, y):
+    return abs(x - y)
+
+
+def dynamic_time_warping(time_series_1, time_series_2):
+    return DTW(time_series_1, time_series_2, linear_distance)
+
+
+class DTW_Exception(BaseException):
+    pass
+
+
+def DTW(time_series_1, time_series_2, distance_func):
+    if not len(time_series_1) or not len(time_series_2):
+        raise DTW_Exception
+    dynamic_warping_cost = []
+    for _ in range(len(time_series_1)):  # create dynamic array
+        dynamic_warping_cost.append([0] * len(time_series_2))
+    dynamic_warping_cost[0][0] = distance_func(time_series_1[0], time_series_2[0])
+    for i in range(1, len(time_series_1)):  # fill first column
+        dynamic_warping_cost[i][0] = dynamic_warping_cost[i - 1][0] +\
+            distance_func(time_series_1[i], time_series_2[0])
+    for i in range(1, len(time_series_2)):  # fill first row
+        dynamic_warping_cost[0][i] = dynamic_warping_cost[0][i - 1] +\
+            distance_func(time_series_1[0], time_series_2[i])
+    # main loop - fill all dynamic  array
+    for x in range(1, len(time_series_1)):
+        for y in range(1, len(time_series_2)):
+            dynamic_warping_cost[x][y] =\
+                distance_func(time_series_1[x], time_series_2[y]) +\
+                    min(
+                        dynamic_warping_cost[x][y - 1],
+                        dynamic_warping_cost[x - 1][y - 1],
+                        dynamic_warping_cost[x - 1][y],
+                    )
+    restored_path = _dtw_restore_path(dynamic_warping_cost)
+    result_cost = dynamic_warping_cost[-1][-1]
+    if DEBUG_PRINT_DYNAMIC_ARR:
+        for line in dynamic_warping_cost:
+            print(line)
+    return restored_path, result_cost
+
+
+def _dtw_restore_path(dynamic_warping_cost):
+    x = len(dynamic_warping_cost) - 1
+    y = len(dynamic_warping_cost[0]) - 1
+    restored_path = [[x, y]]
+    # restore path from dynamic array - find min elements
+    while x > 0 or y > 0:
+        previous_x = x - 1
+        previous_y = y - 1
+        min_elem = None
+        # check up left element first
+        if y >= 1 and x >= 1 and (min_elem is None or
+                dynamic_warping_cost[x - 1][y - 1] < min_elem):
+            min_elem = dynamic_warping_cost[x - 1][y - 1]
+            previous_x, previous_y = x - 1, y - 1
+        # check up element
+        if x >= 1 and (min_elem is None or
+                dynamic_warping_cost[x - 1][y] < min_elem):
+            min_elem = dynamic_warping_cost[x - 1][y]
+            previous_x, previous_y = x - 1, y
+        # check left element
+        if y >= 1 and (min_elem is None or
+                dynamic_warping_cost[x][y - 1] < min_elem):
+            min_elem = dynamic_warping_cost[x][y - 1]
+            previous_x, previous_y = x, y - 1
+        x, y = previous_x, previous_y
+        restored_path.append([x, y])
+    restored_path.reverse()
+    return restored_path
+    
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
---
+++ algo_1/seminar_hm/tests.py
@@ -0,0 +1,70 @@
+import unittest
+import random
+import dtw
+
+
+
+class DTW_Tester(unittest.TestCase):
+
+    def test_base(self):
+        s1 = [1, 2, 3, 4, 5, 5, 5, 4]
+        s2 = [3, 4, 5, 5, 5, 4]
+        result = ([[0, 0], [1, 0], [2, 0], [3, 1], [4, 2], [5, 3], [6, 4], [7, 5]], 3)
+        self.assertEqual(dtw.dynamic_time_warping(s1, s2), result)
+
+    def test_equal(self):
+        s1 = [1, 2, 3, 4, 5, 5, 5, 4]
+        result = ([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]], 0)
+        self.assertEqual(dtw.dynamic_time_warping(s1, s1), result)
+        s2 = [3, 4, 5, 5, 5, 4]
+        result = ([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]], 0)
+        self.assertEqual(dtw.dynamic_time_warping(s2, s2), result)
+    
+    def test_one_num(self):
+        s1 = [1]
+        s2 = [1]
+        result = ([[0, 0]], 0)
+        self.assertEqual(dtw.dynamic_time_warping(s1, s2), result)
+        s1 = [1000]
+        s2 = [1]
+        result = ([[0, 0]], 999)
+        self.assertEqual(dtw.dynamic_time_warping(s1, s2), result)
+    
+    def test_empty(self):
+        s1 = []
+        s2 = [1]
+        with self.assertRaises(dtw.DTW_Exception):
+            dtw.dynamic_time_warping(s1, s2)
+        s1 = [1]
+        s2 = []
+        with self.assertRaises(dtw.DTW_Exception):
+            dtw.dynamic_time_warping(s1, s2)
+        s1 = []
+        s2 = []
+        with self.assertRaises(dtw.DTW_Exception):
+            dtw.dynamic_time_warping(s1, s2)
+    
+    def test_random(self):
+        for _ in range(100):
+            size = random.randrange(3, 100)
+            s1 = []
+            start = random.randrange(-100, 100)
+            step = random.randrange(1, 10)
+            finish = start + step * size
+            for i in range(start, finish, step):
+                s1.append(i)
+            s2 = s1.copy()
+            deleted_index = random.randrange(1, size - 1)
+            s2.pop(deleted_index)
+            result = [[x, x if x < deleted_index and deleted_index != len(s1) - 1  else x - 1] for x in range(size)]
+            self.assertEqual(dtw.dynamic_time_warping(s1, s2)[0], result)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
